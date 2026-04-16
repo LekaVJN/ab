8cf1796ff4445ef0e8cdd52efa1b2f2d935e90e7
Project Author <author@example.com>
2026-04-16 13:26:09 +0700

    Fix Next GitHub Pages config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   basePath: '/ab',
-  assetPrefix: '/ab',
+  assetPrefix: '/ab/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
